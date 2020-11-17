203efe2c4c040d314ec0c7081852ada57054743c
Vincentius <vincent1990@126.com>
2020-11-17 16:24:12 +0800

    3

--- simplelayout/__init__.py
+++ simplelayout/__init__.py
@@ -1,5 +1,5 @@
 from simplelayout.generator.utils import save_fig, save_matrix, make_dir
-from generator.core import generate_matrix
+from simplelayout.generator.core import generate_matrix
 from simplelayout.cli import get_options  # TODO: 保证不修改本行也可以正确导入
 import sys
 sys.path.append('..')
